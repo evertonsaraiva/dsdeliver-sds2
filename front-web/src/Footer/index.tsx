b98dffbe422fdb40f5e324fb3a3ee70ccf15fec6
Everton <evertonsaraiva@gmail.com>
2021-02-16 15:56:22 -0300

    Finish front-web

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,25 +1,25 @@
 import './styles.css';
 import { ReactComponent as YouTubeIcon } from './youtube.svg';
 import { ReactComponent as LinkedinIcon } from './linkedin.svg';
-import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as IstagramIcon } from './instagram.svg';
 
-function Footer(){
-    return (
-       <footer className="main-footer">
-           App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
-           <div className="footer-icons">
-               <a href="http://www.youtube.com/c/DevSuperior" target="_new">
-                    <YouTubeIcon />
-               </a>
-               <a href="http://www.linkedin.com/school/devsuperior" target="_new">
-                   <LinkedinIcon />
-               </a>
-               <a href="http://www.instagram.com/devsuperior.ig">
-                   <InstagramIcon />
-               </a>
-           </div>
-       </footer>
-    )
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+          <YouTubeIcon />
+        </a>
+        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+          <LinkedinIcon />
+        </a>
+        <a href="https://www.instagram.com/devsuperior.ig/">
+          <IstagramIcon />
+        </a>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer;
